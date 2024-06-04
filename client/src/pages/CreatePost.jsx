ff4c2ea546bd4efb0b5dec1f1719051a511d7ed9
Jack Vincent Pobre <jackvincentsanchez.pobre@bicol-u.edu.ph>
2024-06-04 19:10:48 +0800

    initial commit

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import {preview} from '../assets'
+import  { getRandomPrompt } from '../utils'
+import {FormField, Loader} from '../components'
+
+const CreatePost = () => {
+
+
+  return (
+    <div>
+      Create Post
+    </div>
+  )
+}
+
+export default CreatePost
